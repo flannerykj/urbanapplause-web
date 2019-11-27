d64ed310b613b48c4951cd4d427022ef2476ff33
Flannery Jefferson <flannj@gmail.com>
2019-11-26 22:26:39 -0500

    update copy and links on 404 page

--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class ErrorPage extends Component {
   render() {
     return(
       <div>
         <h1 className='title is-1'>404</h1>
-        <h2 className='subtitle is-2'>Oops, that page doesn't exist. Try going to the <a href='/works'>works</a> or <a href='/artist'>artists</a> index.</h2>
+        <h2 className='subtitle is-2'>Oops, that page doesn't exist.</h2>
+        <p>Go back to the <Link to='/'>main page?</Link></p>
       </div>
     )
   }
