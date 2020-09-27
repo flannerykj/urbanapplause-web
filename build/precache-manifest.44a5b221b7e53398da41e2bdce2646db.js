2d65efed56f782fcc053de63100254d5d4bffca4
Flannery Jefferson <flannj@gmail.com>
2020-09-27 17:57:23 -0400

    npm updates and build

---
+++ build/precache-manifest.44a5b221b7e53398da41e2bdce2646db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd22b03fd1a17d5b92accac9667cbbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57df191018da75a5498e",
+    "url": "/static/css/2.455cd81f.chunk.css"
+  },
+  {
+    "revision": "dc01a8af8a66a947b355",
+    "url": "/static/css/main.f33df6e9.chunk.css"
+  },
+  {
+    "revision": "57df191018da75a5498e",
+    "url": "/static/js/2.e161922d.chunk.js"
+  },
+  {
+    "revision": "ec3deb2ce8834f74d09eb8e2bf538a10",
+    "url": "/static/js/2.e161922d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc01a8af8a66a947b355",
+    "url": "/static/js/main.33716d27.chunk.js"
+  },
+  {
+    "revision": "3c8264c4ad27fda28c4b",
+    "url": "/static/js/runtime-main.edcb709e.js"
+  },
+  {
+    "revision": "d6f9d63b9bb2e1e91936b53b24894994",
+    "url": "/static/media/applaud.d6f9d63b.svg"
+  },
+  {
+    "revision": "7d165d2adb652a07b650fcbfab32bd81",
+    "url": "/static/media/ios_app_store_button_fr.7d165d2a.png"
+  },
+  {
+    "revision": "ff5030fdc6c1068dcdea1a3d8441bec2",
+    "url": "/static/media/iphone_app.ff5030fd.png"
+  },
+  {
+    "revision": "0cebb71a541a63e038f0cfb36150734b",
+    "url": "/static/media/ua-logo.0cebb71a.png"
+  }
+]);
